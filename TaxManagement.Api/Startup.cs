18bc6d8846be15de2917099be64969206b78aa22
deepakreddy2011 <deepakreddy2011@gmail.com>
2020-08-19 01:16:25 +0530

    added swagger configuration for API testing

--- TaxManagement.Api/Startup.cs
+++ TaxManagement.Api/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 using TaxManagement.Core;
 using TaxManagement.Infrastructure;
 
@@ -38,6 +39,22 @@ namespace TaxManagement.Api
                 sp.GetRequiredService<IOptions<TaxDBSettings>>().Value);
 
             services.AddControllers();
+            
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Tax Managment API",
+                    Description = "A simple API which manage tax by Muncipalities",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Deepak Reddy",
+                        Email = "deepakreddy2011@gmail.com",
+                        Url = new Uri("https://github.com/deepakreddy2011/TaxManagement"),
+                    }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,6 +66,14 @@ namespace TaxManagement.Api
             }
 
             app.UseHttpsRedirection();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Tax Management API");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseRouting();
 
