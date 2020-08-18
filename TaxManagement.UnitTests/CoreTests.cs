ea52925351c5b69dc61f09ff6f018a017778eb18
deepakreddy2011 <deepakreddy2011@gmail.com>
2020-08-18 20:37:00 +0530

    added unit test for Tax Service in Core

--- TaxManagement.UnitTests/CoreTests.cs
+++ TaxManagement.UnitTests/CoreTests.cs
@@ -1,10 +1,31 @@
-﻿using System;
+﻿using Moq;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using TaxManagement.Core;
 
 namespace TaxManagement.UnitTests
 {
     public class CoreTests
     {
+        private Mock<IRepository> mockRepository;
+
+        [SetUp]
+        public void setup()
+        {
+            this.mockRepository = new Mock<IRepository>();
+        }
+
+        [TestCase]
+        public void GetTaxRateYearlyTest()
+        {
+            this.mockRepository.Setup(x => x.GetTaxRate("Copenhagen", new DateTime(2016, 7, 10))).Returns(0.2m);
+
+            var taxService = new TaxService(this.mockRepository.Object);
+            var taxRate = taxService.GetTaxRateByMunicipalityDate("Copenhagen", new DateTime(2016, 7, 10));
+
+            Assert.AreEqual(taxRate, 0.2m);
+        }
     }
 }
